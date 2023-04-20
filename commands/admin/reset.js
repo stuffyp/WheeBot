1d38faa42abf3baab7a1c786a6eb2c913a9e060f
stuffyp <58758470+stuffyp@users.noreply.github.com>
2023-04-20 06:03:19 +0000

    refactor

--- commands/admin/reset.js
+++ commands/admin/reset.js
@@ -11,44 +11,45 @@ module.exports = {
 	async execute(interaction) {
     const user = interaction.user.id;
     const userData = await getUser(user);
-    if (userData) {
-      const confirm = new ButtonBuilder()
-  			.setCustomId('confirm')
-  			.setLabel('Confirm Reset')
-  			.setStyle(ButtonStyle.Danger);
-  
-  		const cancel = new ButtonBuilder()
-  			.setCustomId('cancel')
-  			.setLabel('Cancel')
-  			.setStyle(ButtonStyle.Secondary);
-  
-  		const row = new ActionRowBuilder()
-  			.addComponents(cancel, confirm);
-  
-  		const response = await interaction.reply({
-  			content: `Are you sure you want to reset your account?`,
-  			components: [row],
-  		});
-
-      try {
-      	const confirmation = await response.awaitMessageComponent({ time: TIMEOUT });
-      
-      	if (confirmation.customId === 'confirm') {
-      		await setUser(user, USER_TEMPLATE);
-      		await confirmation.update({ content: `Account successfully reset.`, components: [] });
-      	} else if (confirmation.customId === 'cancel') {
-      		await confirmation.update({ content: 'Action cancelled.', components: [] });
-      	}
-      } catch (e) {
-      	await interaction.editReply({
-          content: 'Response timed out.',
-          components: [],
-        });
-      }
-    } else {
+    if (userData === null) {
       await interaction.reply({
         content: 'You have not yet registered for an account.',
         ephemeral: true,
+      });
+      return;
+    }
+    
+    const confirm = new ButtonBuilder()
+      .setCustomId('confirm')
+      .setLabel('Confirm Reset')
+      .setStyle(ButtonStyle.Danger);
+
+    const cancel = new ButtonBuilder()
+      .setCustomId('cancel')
+      .setLabel('Cancel')
+      .setStyle(ButtonStyle.Secondary);
+
+    const row = new ActionRowBuilder()
+      .addComponents(cancel, confirm);
+
+    const response = await interaction.reply({
+      content: `Are you sure you want to reset your account?`,
+      components: [row],
+    });
+
+    try {
+      const confirmation = await response.awaitMessageComponent({ time: TIMEOUT });
+    
+      if (confirmation.customId === 'confirm') {
+        await setUser(user, USER_TEMPLATE);
+        await confirmation.update({ content: `Account successfully reset.`, components: [] });
+      } else if (confirmation.customId === 'cancel') {
+        await confirmation.update({ content: 'Action cancelled.', components: [] });
+      }
+    } catch (e) {
+      await interaction.editReply({
+        content: 'Response timed out.',
+        components: [],
       });
     }
 	},
