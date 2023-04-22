c5e0f47bcd6be01865448ac9e21dae3d5cb32c15
stuffyp <58758470+stuffyp@users.noreply.github.com>
2023-04-22 08:19:09 +0000

    add view command for party

--- commands/basic/view.js
+++ commands/basic/view.js
@@ -49,7 +49,7 @@ const executeView = async (interaction) => {
 }
 
 
-const executeManage = async (interaction) => {
+const executeDetailed = async (interaction) => {
   const user = interaction.user.id;
   await sortUser(user, interaction.options.getString('sort_by'));
   const userData = await getUser(user);
@@ -94,13 +94,60 @@ const executeManage = async (interaction) => {
 }
 
 
+const executeParty = async (interaction) => {
+  const user = interaction.user.id;
+  const userData = await getUser(user);
+  if (userData === null) {
+    await interaction.reply('Please register an account first.');
+    return;
+  }
+
+  if (userData.version !== VERSION_NUMBER) {
+    await interaction.reply('Please use the update command to update to the latest version of the game.');
+    return;
+  }
+  if (userData.party.length === 0) {
+    await interaction.reply('Your party is empty.');
+    return;
+  }
+
+  const party = userData.party.map((fullID) => userData.collection.find((c) => c.fullID == fullID));
+  let cardIndex = 0;
+  const maxCardIndex = party.length - 1;
+  const message = await interaction.reply({
+    embeds: [fullDisplay(party[cardIndex], cardIndex, party.length)],
+    fetchReply: true,
+  });
+  message.react('⏮️')
+    .then(() => message.react('⬅️'))
+    .then(() => message.react('➡️'))
+    .then(() => message.react('⏭️'))
+    .catch(error => console.error('One of the emojis failed to react:', error));
+  const filter = (reaction, user) => {
+    return user.id === interaction.user.id && NAV_EMOJIS.includes(reaction.emoji.name);
+  };
+  const collector = message.createReactionCollector({ filter, time: TIME_LIMIT });
+
+  collector.on('collect', (reaction) => {
+    cardIndex = handleNav(reaction, cardIndex, maxCardIndex);
+    interaction.editReply({
+      embeds: [fullDisplay(party[cardIndex], cardIndex, party.length)],
+    });
+  });
+}
+
+
 module.exports = {
 	data: new SlashCommandBuilder()
-		.setName('collection')
-    .setDescription('View your collection')
+		.setName('view')
+    .setDescription('View your cards')
     .addSubcommand(subcommand =>
   		subcommand
-  			.setName('view')
+  			.setName('party')
+  			.setDescription('View your party'))
+    .addSubcommand(subcommand =>
+  		subcommand
+  			.setName('collection')
   			.setDescription('View your collection')
         .addStringOption(option =>
       		option.setName('sort_by')
@@ -115,7 +162,7 @@ module.exports = {
     .addSubcommand(subcommand =>
   		subcommand
   			.setName('detailed')
-  			.setDescription('Manage your collection')
+  			.setDescription('View your collection in detail')
         .addStringOption(option =>
       		option.setName('sort_by')
       			.setDescription('Method to sort by')
@@ -127,10 +174,18 @@ module.exports = {
               { name: 'Level (reverse)', value: SortBy.Level_r },
       			))),
 	async execute(interaction) {
-    if (interaction.options.getSubcommand() === 'view') {
-      executeView(interaction);
-    } else {
-      executeManage(interaction);
+    switch (interaction.options.getSubcommand()) {
+      case 'collection':
+        executeView(interaction);
+        break;
+      case 'detailed':
+        executeDetailed(interaction);
+        break;
+      case 'party':
+        executeParty(interaction);
+        break;
+      default:
+        console.error(`An unknown subcommand was registered: ${interaction.options.getSubcommand()}`);
     }
 	},
 };
