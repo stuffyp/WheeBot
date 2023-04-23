3464a31ee7daafc9700dabea554f46a66dc8248b
Replit user <>
2023-04-23 05:00:17 +0000

    added event handler code

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ const client = new Client({ intents: [
   GatewayIntentBits.GuildMessageReactions,
 ] });
 
+
 client.commands = new Collection();
 
 const foldersPath = path.join(__dirname, 'commands');
@@ -32,31 +33,19 @@ for (const folder of commandFolders) {
 }
 
 
-client.on('ready', ()=> {
-  console.log(`Logged in as ${client.user.tag}!`)
-})
+const eventsPath = path.join(__dirname, 'events');
+const eventsFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
 
-client.on(Events.InteractionCreate, async interaction => {
-	if (!interaction.isChatInputCommand()) return;
+for (const file of eventsFiles) {
+  const filePath = path.join(eventsPath, file);
+  const event = require(filePath);
+  if (event.once) {
+    client.once(event.name, (...args) => event.execute(...args));
+  } else {
+    client.on(event.name, (...args) => event.execute(...args));
+  }
+}
 
-	const command = interaction.client.commands.get(interaction.commandName);
-
-	if (!command) {
-		console.error(`No command matching ${interaction.commandName} was found.`);
-		return;
-	}
-
-	try {
-		await command.execute(interaction);
-	} catch (error) {
-		console.error(error);
-		if (interaction.replied || interaction.deferred) {
-			await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
-		} else {
-			await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
-		}
-	}
-});
 
 init().then(() => {
   client.login(process.env.TOKEN);
