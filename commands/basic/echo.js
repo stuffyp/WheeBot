6b5468f5cb2fc6cb3456c4e5f259909aed2da61d
Replit user <>
2023-04-23 04:47:09 +0000

    echo changes

--- commands/basic/echo.js
+++ commands/basic/echo.js
@@ -1,10 +1,26 @@
 const { SlashCommandBuilder } = require('discord.js');
 
+const data = new SlashCommandBuilder()
+	.setName('echo')
+	.setDescription('Echo a message.')
+	.addStringOption(option =>
+		option
+			.setName('input')
+			.setDescription('The input to echo')
+			.setRequired(true))
+  .addBooleanOption(option =>
+    option
+      .setName('ephemeral')
+      .setDescription('If the message should be private'))
+
+const execute = async (interaction) => {
+  await interaction.reply({
+    content: `Hi ${interaction.user.username}, ${interaction.options.getString('input')}.`,
+    ephemeral: interaction.options.getBoolean('ephemeral')
+  });
+}
+
 module.exports = {
-	data: new SlashCommandBuilder()
-		.setName('echo')
-		.setDescription('Replies to the user'),
-	async execute(interaction) {
-		await interaction.reply(`Hello, ${interaction.user.username}!`);
-	},
+	data,
+	execute
 };
