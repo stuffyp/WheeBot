6b75795ad7606d3b663f561f08485f1dfebd04b7
stuffyp <58758470+stuffyp@users.noreply.github.com>
2023-04-25 06:10:44 +0000

    view inventory

--- dump-database.js
+++ dump-database.js
@@ -2,7 +2,7 @@ const Database = require("@replit/database");
 const db = new Database();
 
 db.list().then(async (keys) => {
-  for (key of keys) {
+  for (const key of keys) {
     console.log(key);
     const val = await db.get(key);
     console.log(val);
