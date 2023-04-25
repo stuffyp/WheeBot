06c052417303a6f84ee7debf66331c2237076c2c
willwin4sure <53180146+willwin4sure@users.noreply.github.com>
2023-04-25 05:04:35 +0000

    made a give card command

---
+++ commands/basic/give-card.js
@@ -0,0 +1,58 @@
+const { SlashCommandBuilder } = require('discord.js');
+const { getUser, updateUser } = require("../../manage-user.js");
+const { getCard, getID } = require("../../cards/read-cards.js");
+const { display } = require("../../cards/ui.js");
+const {
+  VERSION_NUMBER,
+  COLLECTION_SIZE,
+  CARD_DB_TEMPLATE,
+} = require("../../util/constants.js");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('give-card')
+    .setDescription('Give yourself a specific card')
+    .addStringOption(option =>
+      option
+        .setName('card_name')
+        .setDescription('The card you want.')
+        .setRequired(true)),
+  async execute(interaction) {
+    const user = interaction.user.id;
+    const userData = await getUser(user);
+    if (userData === null) {
+      await interaction.reply('Please register an account first.')
+      return;
+    }
+    if (userData.version !== VERSION_NUMBER) {
+      await interaction.reply('Please use the update command to update to the latest version of the game.');
+      return;
+    }
+
+    await updateUser(user, async (userData) => {
+      if (userData.collection.length >= COLLECTION_SIZE) {
+        await interaction.reply('Your collection is already full.');
+        return null;
+      }
+
+      const cardId = getID(interaction.options.getString('card_name').trim().toLowerCase());
+
+      if (!cardId) {
+        await interaction.reply('Card does not exist (try checking your spelling).')
+        return null;
+      }
+
+      const card = getCard(cardId);
+      
+
+      let text = `You have been given a ${card.name}!`;
+
+      await interaction.reply({ content: text, embeds: [display(card)], });
+      userData.idSeed++;
+
+      userData.collection.push(CARD_DB_TEMPLATE(cardId, userData.idSeed))
+
+      return userData;
+    });
+  },
+};
