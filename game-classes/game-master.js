a3149f67aeb99d94644d42b468f0add85fd5de15
Eric Yang <ericyang800@gmail.com>
2023-05-08 18:29:10 -0400

    bug fix?

--- game-classes/game-master.js
+++ game-classes/game-master.js
@@ -155,9 +155,9 @@ module.exports = class GameMaster {
   #cleanUpKO(userId) {
     // be aware that in JS, const subs = this.units[userId] apparently doesn't update when units is modified???
     const field = this.activeUnits[userId].filter(u => !(u.summoned && u.unit.knockedOut()));
-    const subs = this.units[userId];
+    let subs = this.units[userId];
     while (subs.length && field.some((u) => u.unit.knockedOut())) {
-      // console.error(subs, field);
+      subs = this.units[userId];
       const outUnit = field.find(u => u.unit.knockedOut());
       this.substitute(userId, outUnit.fullID, subs[randInt(subs.length)].fullID);
       this.graveyard[userId].push(this.units[userId].pop()); // relies on assumption that substitute pushes to end
